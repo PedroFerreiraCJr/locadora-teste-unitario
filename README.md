0e6819042d2db2e25a9b28fb2a6fca2a1d954808
Pedro <pedroferreiracjr@gmail.com>
2022-04-05 23:07:40 -0300

    aula 2 - usando o framework junit (introdução)

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
  - Repeatable: O R vem de repetitível, a qualquer momento que for executado deve dar o mesmo resultado;
  - Self-Verifying: O próprio teste deve saber quando o resultado foi com sucesso, falha ou erro;
  - Timely: Este vem de oportuno. OU seja um teste deve ser criado no momento oportúnuo;
+
+## Frameworks XUnit
+ Estes frameworks tem este nome por conta no framework SUnit desenvolvido na linguagem de programação Smalltalk, criado por Kent Beck, 1998. Todos estes frameworks tentam seguir um mesmo componente principal de sua arquitetura, e são compostos de alguns componentes:
+ - TestRunner: Responsável por executar os testes e coletar os resultados;
+ - TestFixture (TestContext): Que são as precondições necessárias aos testes;
+ - TestSuites: Permite que sejam agrupados um conjunto de testes que devem ser executados em conjunto;
+ - TestResultFormatter: Quem formata a saída dos testes;
+ - Assertions: São verificadores de estado do teste;
+
+O JUnit também pode ser utilizado para testes de integração e testes funcionais como ferramenta de padronização.
+
+## Metáfora da Janela de Vidro
+Ela foi introduzida em 1982 na área de criminologia, e tenta explicar uma característica do comportamento humano. Considere um edifício com algumas janelas quebradas, se as janelas não forem reparadas, a tendencia é que vândalos quebrem mais janelas.
+Portanto, é sugerido que sempre seja deixado os testes com a barra verde, pois assim, evitasse que mais testes com barra vermelha se acumulem.
--- src/main/java/br/ce/wcaquino/teste_unitario/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/teste_unitario/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.teste_unitario.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.teste_unitario.entidades.Filme;
 import br.ce.wcaquino.teste_unitario.entidades.Locacao;
 import br.ce.wcaquino.teste_unitario.entidades.Usuario;
@@ -29,18 +32,28 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * <pre>
+	 * 	1º O primeio passo para tornar este código um teste é torná-lo um método
+	 * público e com retorno void.
+	 * 	2º O segundo passo é anotá-lo com @Test do pacote org.junit.
+	 * 	3º O terceiro passo a ser realizado é fazer uso das assertions disponíveis
+	 * para validar o resultado da execução do método que está sendo testado.
+	 * </pre>
+	 */
+	@Test
+	public void teste() {
 		// cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
 		Filme filme = new Filme("Filme 1", 2, 5.0);
-		
+
 		// ação
 		Locacao locacao = service.alugarFilme(usuario, filme);
-		
+
 		// verificação
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
