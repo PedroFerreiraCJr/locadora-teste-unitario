e78d7b0684014a8850c4afc978bec58a6ba3e9d4
Pedro Ferreira <pedroferreiracjr@gmail.com>
2024-11-25 18:53:22 -0300

    style: correção na formatação

--- src/test/java/br/ce/wcaquino/teste_unitario/servicos/AssertTest.java
+++ src/test/java/br/ce/wcaquino/teste_unitario/servicos/AssertTest.java
@@ -10,30 +10,33 @@ public class AssertTest {
 	public void test() {
 		Assert.assertTrue(true);
 		Assert.assertFalse(false);
-		
-		Assert.assertEquals(1, 1);	// lê-se: é experado que o valor seja 1, avaliando o valor do segundo argumento;
-		Assert.assertEquals(0.51, 0.51, 0.01);	// o terceiro argumento é um delta de variação (margem de erro);
-		Assert.assertEquals(Math.PI, 3.14, 0.01);	// como não quero precisar colocar o valor com total precisão, é feito a declaração do delta;
-		
+
+		Assert.assertEquals(1, 1); // lê-se: é experado que o valor seja 1, avaliando o valor do segundo argumento;
+		Assert.assertEquals(0.51, 0.51, 0.01); // o terceiro argumento é um delta de variação (margem de erro);
+		Assert.assertEquals(Math.PI, 3.14, 0.01); // como não quero precisar colocar o valor com total precisão, é feito
+													// a declaração do delta;
+
 		int i = 5;
 		Integer i2 = 5;
 //		Assert.assertEquals(i, i2);	// não compila, pois é preciso usar tipos compatíveis, primitivo com primitivo, ou o contrário;
-		
+
 		Assert.assertEquals("Bola", "Bola");
-		Assert.assertEquals("Bola".toLowerCase(), "bola");	// é preciso comparar usando o valor em caixa baixa com caixa baixa
+		Assert.assertEquals("Bola".toLowerCase(), "bola"); // é preciso comparar usando o valor em caixa baixa com caixa
+															// baixa
 		Assert.assertNotEquals("Bola", "Casa");
 		Assert.assertTrue("Bola".startsWith("Bo"));
-		
-		// no caso de objetos de outras classes, quem deve dizer se eles são iguais é a própria classe usando o método equals
+
+		// no caso de objetos de outras classes, quem deve dizer se eles são iguais é a
+		// própria classe usando o método equals
 		Usuario u1 = new Usuario("Usuário 1");
 		Usuario u2 = new Usuario("Usuário 1");
 		Usuario u3 = u2;
 		Assert.assertEquals(u1, u2);
-		
+
 		// para comparar se dois objetos são a mesma instância, use
 		Assert.assertSame(u2, u3);
-		Assert.assertNotSame(u1, u2);	// valida o contrário
-		
+		Assert.assertNotSame(u1, u2); // valida o contrário
+
 		u3 = null;
 		Assert.assertTrue(u3 == null);
 		Assert.assertNull(u3);
