af590d32a8792b0cd426cae77e230d223e206883
Pedro Ferreira <pedroferreiracjr@gmail.com>
2024-12-11 20:39:12 -0300

    aula 21 - apresentação de suites de teste

---
+++ src/test/java/br/ce/wcaquino/teste_unitario/suites/SuiteExecucao.java
@@ -0,0 +1,17 @@
+package br.ce.wcaquino.teste_unitario.suites;
+
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.teste_unitario.servicos.CalculadoraTest;
+import br.ce.wcaquino.teste_unitario.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.teste_unitario.servicos.LocacaoServiceTest;
+
+//@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+
+}
